0b94dd797b5bd6929c2b92b7a070e440c2031477
Narek Maloyan <narek1110@gmail.com>
2022-11-22 17:40:31 +0300

    clip

--- imagededup/utils/data_generator.py
+++ imagededup/utils/data_generator.py
@@ -1,16 +1,19 @@
+import os
 from pathlib import PurePath
 from typing import Callable, Dict, List, Optional, Tuple
 
+import clip
 import numpy as np
 import timm
 import torch
 import torch.nn as nn
+from clip.clip import _MODELS, _download
 from torch.utils.data import DataLoader, Dataset
 from torchvision import models
 
 from imagededup.utils.general_utils import generate_files
 from imagededup.utils.image_utils import load_image
-
+from torchvision import transforms
 
 class ImgDataset(Dataset):
     def __init__(
@@ -76,14 +79,32 @@ class CNNModel(torch.nn.Module):
             mobilenet = (
                 models.mobilenet_v3_small(pretrained=True).eval().to(self.device)
             )
-            self.mobilenet_gap_op = torch.nn.Sequential(mobilenet.features, mobilenet.avgpool)
+            self.mobilenet_gap_op = torch.nn.Sequential(
+                mobilenet.features, mobilenet.avgpool
+            )
+        elif self.name == "ViT-L/14@336px":
+            self.clip_norm = nn.Sequential(
+                transforms.Resize(size=[336, 336]),
+                transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711]),
+            )
+            model_path = _download(
+                _MODELS[self.name], os.path.expanduser("~/.cache/clip")
+            )
+            with open(model_path, "rb") as opened_file:
+                self.clip_vit = torch.jit.load(opened_file).visual.eval().to(self.device)
+            self.avgpool = nn.AdaptiveAvgPool2d(1)
         else:
-            self.timm_model = timm.create_model(self.name, pretrained=True).eval().cuda()
+            self.timm_model = (
+                timm.create_model(self.name, pretrained=True).eval().cuda()
+            )
             self.avgpool = nn.AdaptiveAvgPool2d(1)
 
     def forward(self, x) -> torch.tensor:
         if self.name == "mobilenet_v3_small":
             return self.mobilenet_gap_op(x.to(self.device)).detach().cpu()
+        elif self.name == "ViT-L/14@336px":
+            x = self.clip_norm(x / 255.0)
+            return self.avgpool(self.clip_vit(x.half().to(self.device))).detach().cpu()
         else:
             return (
                 self.avgpool(self.timm_model.forward_features(x.to(self.device)))
